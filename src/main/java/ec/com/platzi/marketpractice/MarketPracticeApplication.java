185b04f09eaad0b9a6beae4f2afb38c45f6ccf7a
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-06-16 22:20:19 -0500

    Initial commit

---
+++ src/main/java/ec/com/platzi/marketpractice/MarketPracticeApplication.java
@@ -0,0 +1,13 @@
+package ec.com.platzi.marketpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/com/platzi/marketpractice/MarketPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.com.platzi.marketpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
